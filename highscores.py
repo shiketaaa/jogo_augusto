3ea380d1921f387ae1d145089039657070fe9b47
shiketaaa <135331101+shiketaaa@users.noreply.github.com>
2023-11-12 15:51:11 -0300

    alteracoes do jogo

--- highscores.py
+++ highscores.py
@@ -1,17 +1,24 @@
-from jogo import pontos
 def abrir_arquivo (op):
     return open('pontuacoes.txt',f'{op}')
 
-arq = abrir_arquivo('a')
-arq.write(pontos + '\n')
-arq.close
-arquivo = abrir_arquivo('r')
-x = arquivo. readlines()
-arquivo.close
-y = []
-for i in range (0,len(x)):
-    y.append(x[i])
-y.sort(reverse= True)
-print (*y)
+def SalvarPontos(pontos):
+    arq = abrir_arquivo('a')
+    arq.write(str(pontos)+"\n")
+    arq.close
+
+def GetPontos():
+    arquivo = abrir_arquivo('r')
+    x = arquivo. readlines()
+    numeros = [int(numero) for numero in x]
+    numerosordenados = sorted(numeros, reverse= True)
+    for i in range (5):
+        print (numerosordenados[i])
+
+    # y = []
+    # for i in range (0,len(x)):
+    #     y.append(x[i])
+    # y.sort(reverse= True)
+    # print (*y)
+    arquivo.close
 # lista = [6,5,7,3,4]
 # print (sorted(lista))
--- jogo.py
+++ jogo.py
@@ -2,95 +2,107 @@ import os
 import WConio2 as WConio
 import cursor
 import random
-# from __main__ import player
+import highscores as hs
+import keyboard
+def RodarJogo():
+    os.system('cls')
+    cursor.hide()
+    voce =  [['‗▄█▄‗']]
+    obstaculo = '╣▄▄▄╠'
+    barreira = '±¶±¶±'
+    bloqueio = '¯‗¯‗¯'
 
-os.system('cls')
-cursor.hide()
-voce =  [['‗▄█▄‗'],]
-obstaculo = '╣▄▄▄╠'
+    #personagem
+    voceI = 9   #linha
+    voceJ = 0  #coluna
+    obstaculoI = 1
+    obstaculoJ = 0
+    barreiraI = 1
+    barreiraJ = 0
+    bloqueioI = 1
+    bloqueioJ = 0
+    simbolo = ''
+    quadros = 500 
 
-# ©▄█▄©
-#  ███
-# ©▀█▀©
-  #personagem
-voceI = 9   #linha
-voceJ = 0  #coluna
-obstaculoI = 1
-obstaculoJ = 0
-simbolo = ''
-quadros = 500 
-
-contador = 0 #controla a velocidade de movimentos
-pontos = 0
-while(simbolo != 'o'):
-    #posicionar o cursor no começo da tela
-    WConio.gotoxy(0,0)
-    
-    if voceI > 10:
-        voceI = 0
-    if obstaculoI >9:
-        obstaculoI = 0
-
-    velocidade = contador % quadros == 0    
-    if velocidade:
+    contador = 0 #controla a velocidade de movimentos
+    pontos = 0
+    while(simbolo != 'o'):
+        #posicionar o cursor no começo da tela
+        WConio.gotoxy(0,0)
         
-        #randomiza a posicao de surgimento dos obstaculos
-        numero = random.randint(0,3)
-        if numero == 0 and obstaculoI == 0:
-            obstaculoJ = 0
-        elif numero == 1 and obstaculoI == 0:
-            obstaculoJ = 3
-        elif numero == 2 and obstaculoI == 0:
-            obstaculoJ = 6
-        elif numero == 3 and obstaculoI == 0:
-            obstaculoJ = 9
-        pontos += 100
-        #mexe obstaculo
-        obstaculoI += 1
+        if voceI > 10:
+            voceI = 0
+        if obstaculoI >9:
+            obstaculoI = 0
 
-    print('*' * 24)  #linha de cima
-    for i in range(10): #10 linhas
+        velocidade = contador % quadros == 0    
+        if velocidade:
+            
+            #randomiza a posicao de surgimento dos obstaculos
+            numero = random.randint(0,3)
+            if numero == 0 and obstaculoI == 0:
+                obstaculoJ = 0
+            elif numero == 1 and obstaculoI == 0:
+                obstaculoJ = 3
+            elif numero == 2 and obstaculoI == 0:
+                obstaculoJ = 6
+            elif numero == 3 and obstaculoI == 0:
+                obstaculoJ = 9
+
+            pontos += 100
+            #mexe obstaculo
+            obstaculoI += 1
+
+        print('*' * 24)  #linha de cima
+        for i in range(10): #10 linhas
+            
+            print('▒', end='')  #abre cada linha
+
+            for j in range(12): #23 colunas
+            
+                if i==voceI and j==voceJ:
+                    for i in range (len(voce)):
+                        print (*voce[i], end= '')
+                elif i ==obstaculoI and j == obstaculoJ:
+                    print (obstaculo, end='')
+                else:
+                    print(' ', end=' ')
+
+
+                bateuobs = obstaculoI == voceI and obstaculoJ == voceJ  
+                #colisao de personagem e obstaculo
+                if bateuobs:
+                    simbolo = "o"
+            
+            print('▒')  #fecha cada linha 
+
+        print('*' * 24) #linha de baixo
+
+        print (f'pontuacao: {pontos}')
+        print ("    desvie das barreiras")
+        contador += 1
         
-        print('▒', end='')  #abre cada linha
 
-        for j in range(12): #23 colunas
-           
-            if i==voceI and j==voceJ:
-                for i in range (len(voce)):
-                    print (*voce[i], end= '')
-            elif i ==obstaculoI and j == obstaculoJ:
-                print (obstaculo, end='')
-            else:
-                 print(' ', end=' ')
+        #pegar interações do usuario
+        if WConio.kbhit():
+            (tecla, simbolo) = WConio.getch()
+            
+            if simbolo=='a' or simbolo=='A':
+                if voceJ>0:
+                    voceJ -= 3
+
+            elif simbolo=='d' or simbolo=='D':
+                if voceJ<9:
+                    voceJ += 3
+            
+            #dobra a velocidade uma vez
+            if simbolo == ' ':
+                quadros = 250
                 
-            #colisao de personagem e obstaculo
-            if obstaculoI == voceI and obstaculoJ == voceJ:
-                simbolo = "o"
-        
-        print('▒')  #fecha cada linha 
+            
+            if quadros <=250 and quadros >= 100:
+                quadros -=10
+    print("Fim do Jogo!")
+    hs.SalvarPontos(pontos)
 
-    print('*' * 24) #linha de baixo
-
-    # print (player)
-    print (f'pontuacao: {pontos}')
-    print ("    desvie das barreiras")
-    contador += 1
     
-
-    #pegar interações do usuario
-    if WConio.kbhit():
-        (tecla, simbolo) = WConio.getch()
-        
-        if simbolo=='a' or simbolo=='A':
-            if voceJ>0:
-                voceJ -= 3
-
-        elif simbolo=='d' or simbolo=='D':
-            if voceJ<9:
-                voceJ += 3
-        
-        #dobra a velocidade uma vez
-        if simbolo == ' ':
-            quadros = 250
-print("Fim do Jogo!")
-# os.system('python . ')
